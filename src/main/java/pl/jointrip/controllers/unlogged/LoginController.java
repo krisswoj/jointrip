0e2bcb20e518e69c7d847742faffd1be036cec13
Arkadiusz Stankiewicz <sughalith@gmail.com>
2018-05-21 11:30:44 +0200

    Refactor kodu

--- src/main/java/pl/jointrip/controllers/unlogged/LoginController.java
+++ src/main/java/pl/jointrip/controllers/unlogged/LoginController.java
@@ -1,4 +1,4 @@
-package pl.jointrip.Controllers;
+package pl.jointrip.controllers.unlogged;
 
 import javax.validation.Valid;
 
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import pl.jointrip.domain.User;
+import pl.jointrip.models.User;
 import pl.jointrip.services.UserService;
 
 @Controller
