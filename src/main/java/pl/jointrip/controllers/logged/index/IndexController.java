1ea4bdc5a71863f8fedc88eb38b62a4c805b859b
Arkadiusz Stankiewicz <sughalith@gmail.com>
2018-07-21 13:28:37 +0200

    error mapping do przetestowania

--- src/main/java/pl/jointrip/controllers/logged/index/IndexController.java
+++ src/main/java/pl/jointrip/controllers/logged/index/IndexController.java
@@ -1,12 +1,13 @@
 package pl.jointrip.controllers.logged.index;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class IndexController {
+public class IndexController implements ErrorController{
 
     @RequestMapping(value={"/","/index"}, method = RequestMethod.GET)
     public ModelAndView index(){
@@ -14,5 +15,19 @@ public class IndexController {
         modelAndView.setViewName("index");
         return modelAndView;
     }
+
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "/error/error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+
 
 }
