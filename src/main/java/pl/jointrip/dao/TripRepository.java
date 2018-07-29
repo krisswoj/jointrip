d0ccfe8d96baf18c7bf7ca89c98bd758ba57590b
Arkadiusz Stankiewicz <sughalith@gmail.com>
2018-07-29 14:28:20 +0200

    Acceptation panel

--- src/main/java/pl/jointrip/dao/TripRepository.java
+++ src/main/java/pl/jointrip/dao/TripRepository.java
@@ -3,6 +3,9 @@ package pl.jointrip.dao;
 import org.springframework.data.repository.CrudRepository;
 import pl.jointrip.models.Trip;
 
+import java.util.Collection;
+
 //@Repository
 public interface TripRepository extends CrudRepository<Trip, Integer> {
+    Collection<Trip> findTripByTripStatus(Integer status);
 }
