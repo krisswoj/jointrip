ba4e493cd0dfa351af544c69ed3b80fbd10e6429
Arkadiusz Stankiewicz <sughalith@gmail.com>
2018-10-01 13:39:17 +0200

    Edycja uzytkownika

--- src/main/java/pl/jointrip/controllers/logged/userInfo/UserInfoController.java
+++ src/main/java/pl/jointrip/controllers/logged/userInfo/UserInfoController.java
@@ -3,27 +3,49 @@ package pl.jointrip.controllers.logged.userInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import pl.jointrip.dao.UserRepository;
 import pl.jointrip.models.Trip;
 import pl.jointrip.models.User;
 import pl.jointrip.services.userService.UserService;
 
-import javax.annotation.PostConstruct;
+import javax.validation.Valid;
 
 @Controller
 public class UserInfoController {
 
     @Autowired
     UserService userService;
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping(value = "/user/edit")
-    public ModelAndView showTrips() {
+    public ModelAndView fetchUserEdit() {
         ModelAndView modelAndView = new ModelAndView();
-        User user = userService.getLoggedUser();
-        modelAndView.addObject("user", user);
+        modelAndView.addObject("userEdit", new User());
         modelAndView.setViewName("user/edit");
         return modelAndView;
     }
+    @PostMapping(value = "/user/edit")
+    public ModelAndView editUser(@Valid User user) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("userEdit", new User());
+        User userToEdit = userService.getLoggedUser();
+        userToEdit.setName(user.getName());
+        userToEdit.setLastName(user.getLastName());
+        userRepository.save(userToEdit);
+        modelAndView.setViewName("user/edit");
+        return modelAndView;
+    }
+    @GetMapping(value = "/user/info")
+    public ModelAndView fetchUserInfo() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("userInfo", userService.getLoggedUser());
+        modelAndView.setViewName("user/info");
+        return modelAndView;
+    }
+
 }
