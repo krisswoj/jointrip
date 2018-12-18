a2e5b956f4a445479d8c41f058761a485bb82d3b
Krzysztof Wojdak <s14642@pjwstk.edu.pl>
2018-12-18 20:27:55 +0100

    added new views to organisator panel

--- src/main/java/pl/jointrip/services/tripService/DailyTripPlanService.java
+++ src/main/java/pl/jointrip/services/tripService/DailyTripPlanService.java
@@ -1,13 +1,16 @@
 package pl.jointrip.services.tripService;
 
 import org.springframework.stereotype.Service;
+import pl.jointrip.models.entities.trip.DailyTripPlan;
 import pl.jointrip.models.entities.trip.Trip;
 import pl.jointrip.models.entities.trip.TripWrapper;
 
 
 public interface DailyTripPlanService {
 
-    public TripWrapper tripWithDailyPlan(Trip trip);
+    TripWrapper tripWithDailyPlan(Trip trip);
 
     Trip findTripById(int tripId);
+
+    DailyTripPlan addNewDailyPlan(DailyTripPlan dailyTripPlan, int tripId);
 }
--- src/main/java/pl/jointrip/services/tripService/impl/DailyTripPlanImpl.java
+++ src/main/java/pl/jointrip/services/tripService/impl/DailyTripPlanImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import pl.jointrip.dao.DailyTripPlanRepository;
 import pl.jointrip.dao.DocumentsRepository;
 import pl.jointrip.dao.TripRepository;
+import pl.jointrip.models.entities.trip.DailyTripPlan;
 import pl.jointrip.models.entities.trip.Trip;
 import pl.jointrip.models.entities.trip.TripWrapper;
 import pl.jointrip.services.tripService.DailyTripPlanService;
@@ -32,8 +33,16 @@ public class DailyTripPlanImpl implements DailyTripPlanService {
     }
 
     @Override
-    public Trip findTripById(int tripId){
+    public Trip findTripById(int tripId) {
         return tripRepository.findTripByTripIdAndTripMember(tripRepository.findById(tripId), userService.getLoggedUser());
+    }
+
+    @Override
+    public DailyTripPlan addNewDailyPlan(DailyTripPlan dailyTripPlan, int tripId) {
+        dailyTripPlan.setTripId(tripRepository.findById(tripId));
+        dailyTripPlan.setStatus(1);
+        dailyTripPlan.setAddeddate(null);
+        return dailyTripPlanRepository.save(dailyTripPlan);
     }
 
 
