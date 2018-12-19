2dd440c4a4058dca8a805587d927b1d0e8a8ad7d
Michał Kunda <michkun22@gmail.com>
2018-12-19 23:39:26 +0100

    testest

--- src/main/java/pl/jointrip/controllers/logged/adminPanel/TripAcceptationController.java
+++ src/main/java/pl/jointrip/controllers/logged/adminPanel/TripAcceptationController.java
@@ -1,16 +1,30 @@
 package pl.jointrip.controllers.logged.adminPanel;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import pl.jointrip.models.entities.trip.Trip;
+import pl.jointrip.models.system.SystemNotification;
 import pl.jointrip.services.tripService.TripAcceptationService;
 
 @Controller
 public class TripAcceptationController {
+
+    @Value("${TRIP_ACCEPTED_POSITIVE}")
+    private String acceptTripPositive;
+
+    @Value("${TRIP_ACCEPTED_NEGATIVE}")
+    private String acceptTripNegative;
+
+    @Value("${TRIP_REJECTED_POSITIVE}")
+    private String rejectTripPositive;
+
+    @Value("${TRIP_REJECTED_NEGATIVE}")
+    private String rejectTripNegative;
 
     @Autowired
     TripAcceptationService acceptationService;
@@ -26,7 +40,9 @@ public class TripAcceptationController {
     @RequestMapping(value = "/admin/acceptationPanel/accept", params = "id", method = RequestMethod.GET)
     public ModelAndView acceptTrip(@RequestParam("id") int id) {
         ModelAndView modelAndView = new ModelAndView();
-        acceptationService.changeTripStatus(id, 1);
+        boolean result = acceptationService.changeTripStatus(id, 1);
+        SystemNotification systemNotification = result ? new SystemNotification("true", acceptTripPositive) : new SystemNotification("fail", acceptTripNegative);
+        modelAndView.addObject("message", systemNotification);
         modelAndView.addObject("notAcceptedTrips", acceptationService.fetchTripsToActivate(0));
         modelAndView.setViewName("admin/acceptation-panel");
         return modelAndView;
@@ -46,7 +62,9 @@ public class TripAcceptationController {
     @RequestMapping(value = "/admin/acceptationPanel/reject", params = "id", method = RequestMethod.GET)
     public ModelAndView rejectTrip(@RequestParam("id") int id) {
         ModelAndView modelAndView = new ModelAndView();
-        acceptationService.changeTripStatus(id, 2);
+        boolean result = acceptationService.changeTripStatus(id, 2);
+        SystemNotification systemNotification = result ? new SystemNotification("true", rejectTripPositive) : new SystemNotification("fail", rejectTripNegative);
+        modelAndView.addObject("message", systemNotification);
         modelAndView.addObject("notAcceptedTrips", acceptationService.fetchTripsToActivate(0));
         modelAndView.setViewName("admin/acceptation-panel");
         return modelAndView;
