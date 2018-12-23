5af545bbd5fe826b929557bea83c3ac7083f0e04
Krzysztof Wojdak <krisswoj@gmail.com>
2018-12-23 16:15:01 +0100

    no message

--- src/main/java/pl/jointrip/JointripApplication.java
+++ src/main/java/pl/jointrip/JointripApplication.java
@@ -19,8 +19,8 @@ public class JointripApplication {
 	@Bean
 	CommandLineRunner init(StorageService storageService) {
 		return (args) -> {
-			storageService.deleteAll();
-			storageService.init();
+//			storageService.deleteAll();
+//			storageService.init();
 		};
 	}
 }
